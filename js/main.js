3ddf49faa035306830147245be09e419c967ff70
Burhan <ahmetilov20@gmail.com>
2023-08-03 02:14:02 +0200

    end

---
+++ js/main.js
@@ -0,0 +1,58 @@
+/* for(let i = 1; i < 136; i++) {
+    console.log(i);
+}; */
+
+
+
+/* for(let i = 1; i < 136; i+=2) {
+    console.log(i);
+}; */
+
+
+
+/* function Sum(n){
+    return n * (n + 1) / 2;
+};
+
+for(let i = 1; i < 136; i++) {
+    console.log(`Number is: ${i} Sum: ${Sum(i)}`);
+}; */
+
+
+
+/* const x = [1, 4, 2, 12];
+
+for(let i = 0 ; i < x.length; i++) {
+    console.log(x[i]);
+}; */
+
+
+
+/* const x = [2, -3, -1];
+
+console.log(Math.max(...x)); */
+
+
+
+/* const x = [2, 1, 3];
+const avrg = x.reduce( (a,b) => a + b, 0) / x.length;
+
+console.log(avrg); */
+
+
+
+/* const x = [2, -1, 4, -3];
+const newX = x.map(num => num < 0 ? 0 : num);
+
+for(let i = 0 ; i < newX.length; i++) {
+    console.log(newX[i]);
+}; */
+
+
+
+/* const x = [1, -4, 0, -1];
+const newX = x.map(num => num < 0 ? "Turing" : num);
+
+for(let i = 0 ; i < newX.length; i++) {
+    console.log(newX[i]);
+}; */
